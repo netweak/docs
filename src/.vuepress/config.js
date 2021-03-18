62e54da7cc33cee0cbca53e473f69d6664236117
Lucas Decrock <lucasdecrock@outlook.com>
2021-03-18 12:30:06 +0100

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'Netweak — Documentation',
+  title: 'Netweak Documentation',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
